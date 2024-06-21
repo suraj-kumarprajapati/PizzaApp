b60d8c9bd1f10ea12a9af5e3fcd02bf985ad3cc5
Suraj Prajapati <sp0640481@gmail.com>
2024-06-21 11:46:33 +0530

    added body parser dependency

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const serverConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
 
@@ -6,13 +7,17 @@ const connectDB = require('./config/dbConfig');
 const PORT = serverConfig.PORT;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.text()); 
+app.use(bodyParser.urlencoded());
+
 
 app.listen(PORT, async () => {
   await connectDB();
   console.log("Server started at PORT : ", PORT);
 });
 
-
+   
 // ip-address:PORT   -> socket address
 // localhost:3000   -> socket address
 // 127.0.0.0:3000   -> socket address
