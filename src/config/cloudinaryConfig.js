b3a15964e97dec7609cb00812fc5ad9b6c6ceca6
Suraj Prajapati <sp0640481@gmail.com>
2024-06-29 17:13:14 +0530

    added cloudinary api

--- src/config/cloudinaryConfig.js
+++ src/config/cloudinaryConfig.js
@@ -8,7 +8,7 @@ const serverConfig = require('./serverConfig');
 cloudinary.config({
     cloud_name : serverConfig.CLOUDINARY_CLOUD_NAME,
     api_key : serverConfig.CLOUDINARY_API_KEY,
-    api_secret : serverConfig.CLOUDINARY_API_KEY
+    api_secret : serverConfig.CLOUDINARY_API_SECRET
 });
 
 
--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const userRouter = require('./routes/userRoute');
 const cartRouter = require('./routes/cartRoute');
 const authRouter = require('./routes/authRoute');
 const { isLoggedIn } = require('./validation/authValidation');
+const uploader = require('./middlewares/multerMiddleware');
+const cloudinary = require('./config/cloudinaryConfig');
+const fs = require('fs');
 
 
 const PORT = serverConfig.PORT; 
@@ -20,7 +23,7 @@ const app = express();
 // app.use(bodyParser.urlencoded());  
 
 
-// middlewares
+// data transformation middlewares
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({extended : true}));
@@ -32,6 +35,8 @@ app.use('/users', userRouter);
 app.use('/carts', cartRouter);
 app.use('/auth', authRouter);
 
+
+// testing middlewares
 app.get('/ping', isLoggedIn, (req, res) => {
   console.log("cookies : ", req.cookies);
   res.json({
@@ -39,19 +44,40 @@ app.get('/ping', isLoggedIn, (req, res) => {
   });
 });
 
+app.post('/photo', uploader.single('pizzaImage'), async (req, res) => {
+  console.log(req.file);
+  console.log(req.file.path);
+  try{
+    const uploadResult = await cloudinary.uploader.upload(req.file.path, {
+      resource_type : 'auto'
+    });
+    console.log(uploadResult);
+    fs.unlink(req.file.path);
+  }
+  catch(error) {
+    console.log(error);
+  }
+ 
+  res.json({
+    message : "OK" 
+  });
+});  
+
+ 
+
+ 
 
 
 app.listen(PORT, async () => {
   await connectDB();
   console.log("Server started at PORT : ", PORT);
 });
-    
-    
+     
 // ip-address:PORT   -> socket address
 // localhost:3000   -> socket address
 // 127.0.0.0:3000   -> socket address
 
 
- 
+  
 
  
